e989d8722d9fda4d796e9dfccfc2c92c22fb573b
kishan Vishwakarma <kapoorscript@gmail.com>
2025-01-28 17:55:07 +0530

    comparison in datatype

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -10,6 +10,41 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = "Hood"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+
+
+// ************************ Operation *************************
+
+let value = 10
+// console.log(-value);
+
+
+// console.log("1"+1); // 11
+// console.log(1+1); // 2
+// console.log(1+1+"2"); // 112
+// console.log("2"+1+1); // 22
+
+
+// console.log(+true) // 1
+// console.log(+true) // 1
+// console.log(+""); // 0
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+// for docs of js 
+https://tc39.es/ecma262/  
 
---
+++ 04_comparison.js
@@ -0,0 +1,19 @@
+// console.log("2" > 1); // true 
+// console.log("02" > 1); // true 
+// console.log("-02" > 1); // false
+
+
+// console.log(null > 0 ); // false
+// console.log(null == 0 ); // false
+// console.log(null >= 0 ); // true
+
+// all false
+// console.log(undefined == 0); 
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// === strick check its check its value with its type also 
+
+console.log("2" === 2); // false
+
