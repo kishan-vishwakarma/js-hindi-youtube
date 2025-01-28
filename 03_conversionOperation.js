3b059b8bf4bcbf6f04971196e4c8d1e9e26836cf
kishan Vishwakarma <kapoorscript@gmail.com>
2025-01-28 17:02:00 +0530

    conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "123ac"
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33acb" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Hood"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
