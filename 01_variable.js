d7c16b352a7566f54e6079aa1378c850409201a0
kishan Vishwakarma <kapoorscript@gmail.com>
2025-01-28 14:57:06 +0530

    vairable

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const  accountId = 12345
+let username = "kishan"
+var password = "kishan@123"
+let fees;
+password = "ravi@123"
+
+console.log(fees);
+
+/*
+prefer not to use var 
+becouse of issue in block scope and fucntional scope
+*/
+console.table([accountId,username,password,fees]);
