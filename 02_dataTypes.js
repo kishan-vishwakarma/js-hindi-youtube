8f35c660d48752cc4740eaea4418b6b9c8bb7973
kishan Vishwakarma <kapoorscript@gmail.com>
2025-01-28 16:28:21 +0530

    data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code  as newer version 
+
+// alert(3+3); we are using node js , not browser
+
+console.log( 3 + 
+
+ 3 );  // code readibility should be high
+
+
+ // number => 2 to power 53
+ // string 
+ // boolean => true/false
+ // null => standalone value 
+ // undefined 
+ // symbol => unique
+ // object 
+ // 
+
+
+ console.log(typeof "Kishan")
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
+
+ 
