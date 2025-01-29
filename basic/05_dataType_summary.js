3d84ad26be72108c2337b5ca09cf84eb5cea5596
kishan Vishwakarma <kapoorscript@gmail.com>
2025-01-29 21:55:49 +0530

    Basic of js

---
+++ basic/05_dataType_summary.js
@@ -0,0 +1,23 @@
+// primitive datatype 
+// 7 Types : string, number, boolean, symbol, null, undefined,BigInt
+const testSymbol = BigInt('123');
+console.log(typeof testSymbol);
+
+
+// non-primitive (Reference )
+
+// array, object, function
+
+const myArr = ["Honda","Hyundayi","Mahindra"] // object
+
+let myObj = { // object
+ Tvs:"Apache",
+ Honde:"spendor"
+}
+
+let myFn = function(){ // function but it also called object function
+ console.log("This is my first Fn in Js");
+ 
+}
+
+// console.log(typeof myArr);
