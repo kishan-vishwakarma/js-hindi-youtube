a9d72b74c0e202884306f43372f074593d0bae36
kishan Vishwakarma <kapoorscript@gmail.com>
2025-01-29 22:20:58 +0530

    stack & heap || call by value & reference

--- basic/05_dataType_summary.js
+++ basic/05_dataType_summary.js
@@ -1,7 +1,7 @@
 // primitive datatype 
 // 7 Types : string, number, boolean, symbol, null, undefined,BigInt
 const testSymbol = BigInt('123');
-console.log(typeof testSymbol);
+// console.log(typeof testSymbol);
 
 
 // non-primitive (Reference )
@@ -21,3 +21,30 @@ let myFn = function(){ // function but it also called object function
 }
 
 // console.log(typeof myArr);
+
+
+//================ Memory *** ===================
+
+// stack (primitive) and heap (reference or non-primitive)
+
+let youtubeName = "HitechChoudharyDotCom";
+
+let anotherName = youtubeName;
+
+anotherName = "ChaiAurCode";
+
+// console.log(youtubeName);
+// console.log(anotherName);
+
+
+let obj = {
+  email:"kishan@gmail.com",
+  mob:"9695116238"
+}
+
+let newMyObj = obj
+
+newMyObj.email = "hitesh@gmail.com"
+
+console.log(obj.email);
+console.log(newMyObj.email);
