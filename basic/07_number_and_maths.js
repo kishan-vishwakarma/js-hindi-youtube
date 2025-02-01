7cbc436e7719144ce8c2310feda02cec92bcea7d
kishan Vishwakarma <kapoorscript@gmail.com>
2025-02-01 11:30:13 +0530

    math and number

---
+++ basic/07_number_and_maths.js
@@ -0,0 +1,27 @@
+const score = 300
+// console.log(typeof score);
+
+const newScore = new Number(123.9899)
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+// console.log(newScore.toPrecision(1));
+
+// console.log(newScore.toExponential(1));
+
+// console.log(newScore.toLocaleString());
+// console.log(newScore.toLocaleString('en-IN'));
+
+//=================== Math ===========================
+
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4.,6,1,0));
+console.log(Math.max(4.,6,1,0));
+
+
