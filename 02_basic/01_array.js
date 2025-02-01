3ebb9ed4e69097029df17a19d4819eac2dabce1a
kishan Vishwakarma <kapoorscript@gmail.com>
2025-02-01 13:15:52 +0530

    array part 1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,38 @@
+const myArr = [1,2,'kishan',true]
+const myHeros = ["Batman","IronMan"]
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(typeof myArr);
+// console.log(typeof myHeros);
+
+
+const newArr = new Array(1,2,true,"komal")
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// Array Method 
+// newArr.push(99)
+// console.log(newArr);
+// console.log(newArr.shift());
+// console.log(newArr.unshift(1));
+
+
+// console.log(newArr);
+// console.log(newArr.join());
+
+// console.log(newArr);
+console.log(newArr.slice(1,3)); // 
+console.log(newArr);
+
+
+console.log(newArr.splice(1,3)); // splice manipute original value 
+console.log(newArr);
+
+
+
+
+
+
+
+
+
