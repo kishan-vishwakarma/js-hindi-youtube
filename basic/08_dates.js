ce686d170d3905592c7f8e27217deb660c56b0d7
kishan Vishwakarma <kapoorscript@gmail.com>
2025-02-01 12:37:37 +0530

    date and time

---
+++ basic/08_dates.js
@@ -0,0 +1,14 @@
+let testDate = new Date();
+// console.log(testDate);
+// console.log(testDate.toDateString());
+// console.log(testDate.toISOString());
+// console.log(testDate.toJSON());
+// console.log(testDate.toLocaleDateString());
+// console.log(testDate.toLocaleString());
+// console.log(testDate.toLocaleTimeString());
+// console.log(testDate.toString());
+// console.log(testDate.toUTCString());
+console.log(typeof testDate);
+
+
+
