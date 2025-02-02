66489b20900e39ddbfcad8cdacc828fab683bdb1
kishan Vishwakarma <kapoorscript@gmail.com>
2025-02-02 08:41:03 +0530

    object distract

--- 02_basic/04_objectPart2.js
+++ 02_basic/04_objectPart2.js
@@ -32,4 +32,29 @@ const obj3 = {...obj1, ...obj2}
 // console.log(Object.keys(userDetails));
 // console.log(Object.values(userDetails));
 // console.log(Object.entries(userDetails));
-console.log(userDetails.hasOwnProperty('namel'));
+// console.log(userDetails.hasOwnProperty('namel'));
+
+// Important array and object distract 
+
+const course = {
+  courseName: "js",
+  courseType: "master",
+  courseInstructor : "Tejas"
+}
+
+
+// course.courseInstructor = "Amit"
+// course.courseType = "Middle"
+// course.courseName = "Js Master"
+
+//alternative and commenly used
+const {courseName} = course
+
+// here i change name also 
+const {courseInstructor: kk} = course
+
+console.log(courseName);
+console.log(kk);
+
+
+
